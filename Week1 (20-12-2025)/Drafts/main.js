a8790d0747980a97e227bc4b747c25c895fdebd3
Yousef <20912024100637@fci.zu.edu.eg>
2025-12-20 18:13:04 +0200

    First week's drafts before doing the tasks

---
+++ Week1 (20-12-2025)/Drafts/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	document.querySelector("h1").style.color = "#1b5c91ff";
+};
+// window.alert("Helloooo");
+// document.write("wowwow");
+// Directive = %c
+console.log("Hello bois");
+console.error("Hello bois");
+console.log("The word %cwassup %cwould be red", "color: red;", "color: unset;");
