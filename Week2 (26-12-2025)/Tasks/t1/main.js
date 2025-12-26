8430cb6c009980bd122d54d44f30250d8e5cf743
Yousef <20912024100637@fci.zu.edu.eg>
2025-12-26 23:19:12 +0200

    1st task week2

---
+++ Week2 (26-12-2025)/Tasks/t1/main.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 20;
+console.log("Normal Concatenate: " + num1 + num2);
+console.log("Normal Concatenate: " + typeof (num1 + num2));
+console.log(`Template Literals: ${num1}${num2}`);
+console.log(`Template Literals: ${typeof (num1 + num2)}`);
+console.log("Normal Concatenate: \n" + num2 + "\n" + num1);
+console.log(`Template Literals:
+${num2}
+${num1}`);
