bc111a23232a2133185c5adb80ce013ad7872c17
Yousef <20912024100637@fci.zu.edu.eg>
2026-01-27 03:25:51 +0200

    Solved The array challenge

--- Week6 (23-1-2026)/Tasks/Array Challenge/main.js
+++ Week6 (23-1-2026)/Tasks/Array Challenge/main.js
@@ -10,10 +10,20 @@ let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
 // Write Code Here
 
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
+console.log(my.slice(zero, my.indexOf("Gamal")).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
+console.log(my.reverse().slice(counter, my[zero].length)); // ["Elham", "Mazero"]
 
-console.log(my.slice("????")); // ["Elham", "Mazero"]
+let el = my
+	.slice(counter, my[zero].length)
+	.shift()
+	.slice(zero, my[zero].length - counter);
+let ze = my
+	.slice(counter, my[zero].length)
+	.pop()
+	.slice(my[zero].length - counter);
+console.log(el + ze); // "Elzero"
 
-console.log(); // "Elzero"
-
-console.log(); // "rO"
+console.log(
+	ze.charAt(my[zero].length - counter) +
+		ze.charAt(my.length - counter).toUpperCase(),
+); // "rO"
