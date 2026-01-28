28667b34472bfac4f9d8364abcd0d895eba3800d
Yousef <20912024100637@fci.zu.edu.eg>
2026-01-28 23:52:56 +0200

    Solved for loop tasks

--- Week7 (28-1-2026)/Tasks/for loop (48-53)/main.js
+++ Week7 (28-1-2026)/Tasks/for loop (48-53)/main.js
@@ -17,3 +17,29 @@ for (let i = start2; i >= stop2; i--) {
 	if (i >= start2) console.log(i);
 	else console.log(`0${i}`);
 }
+
+// T3
+let start3 = 1;
+let end3 = 6;
+let breaker3 = 2;
+
+for (let i = start3; i <= end3; i++) {
+	console.log(i);
+	for (let j = breaker3; j < end3; j += breaker3) {
+		console.log(`-- ${j}`);
+	}
+}
+
+// T4
+let index4 = 10;
+let jump4 = 2;
+
+for (let i = index4; i > jump4; i -= jump4) {
+	console.log(i);
+}
+
+// Output
+// 10
+// 8
+// 6
+// 4
