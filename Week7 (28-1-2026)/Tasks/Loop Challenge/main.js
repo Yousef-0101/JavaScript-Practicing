d49c57827deba47594283282f11d3dc4a461688f
Yousef <20912024100637@fci.zu.edu.eg>
2026-02-01 13:11:27 +0200

    Solved loop challenge

--- Week7 (28-1-2026)/Tasks/Loop Challenge/main.js
+++ Week7 (28-1-2026)/Tasks/Loop Challenge/main.js
@@ -2,9 +2,10 @@
     Loop Challenge
 */
 
-let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
+let myAdmins = ["Ahmed", "Osama", "Sayed", "Yousef", "Stop", "Samera"];
 let myEmployees = [
 	"Amgad",
+	"Yaseen",
 	"Samah",
 	"Ameer",
 	"Omar",
@@ -15,18 +16,27 @@ let myEmployees = [
 ];
 
 let numberOfAdmins = 0;
+let teamCounter = 1;
+let membersCounter;
 
 for (let i = 0; i < myAdmins.length; i++) {
 	if (myAdmins[i] === "Stop") break;
 	numberOfAdmins++;
 }
-
-let team1 = [];
-
-// for (let i = 0; 1 < myEmployees.length; i++) {
-// 	if (myEmployees[i][0] == myAdmins[0][0]) team1.push(myEmployees[i]);
-// }
-
-console.log(team1);
 document.write(`<div>We Have ${numberOfAdmins} Admins</div>`);
-document.write("<hr></hr>");
+document.write(`<hr></hr>`);
+
+for (let i = 0; i < numberOfAdmins; i++) {
+	document.write(`<div>The Admin For Team ${teamCounter} Is ${myAdmins[i]}`);
+	document.write(`<h3>Team Members:</h3>`);
+	teamCounter++;
+	membersCounter = 1;
+	for (let j = 0; j < myEmployees.length; j++) {
+		if (myEmployees[j][0] == myAdmins[i][0]) {
+			document.write(`<p>${membersCounter}- ${myEmployees[j]}</p>`);
+			membersCounter++;
+		} else continue;
+	}
+	document.write(`</div>`);
+	document.write(`<hr></hr>`);
+}
