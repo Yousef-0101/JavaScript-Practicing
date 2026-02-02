4e26538dda79a6137a7d303b3884d5c7ffe2a0a6
Yousef <20912024100637@fci.zu.edu.eg>
2026-02-02 20:32:27 +0200

    more functions

--- Week8 (2-2-2026)/Draft/main.js
+++ Week8 (2-2-2026)/Draft/main.js
@@ -1,5 +1,15 @@
-function Welcome(userName, age, sex) {
-	console.log(`${userName} is a ${age} years old ${sex}`);
+function welcome(userName, age, gender) {
+	console.log(`${userName} is a ${age} years old ${gender}`);
 }
+welcome("Yousef", 20, "male");
 
-Welcome("Yousef", 20, "male");
+function sayHello(userName, age, gender) {
+	let title;
+	if (gender === "male" || gender === "Male") title = "Mr.";
+	else title = "Mrs.";
+	if (age < 16) console.log("Welcome to Roblox Muahahaha");
+	else if (age >= 16 && age < 50)
+		console.log(`Welcome to Roblox ${title}${userName}. Go fishiiinngggg!!!!`);
+	else if (age >= 50) console.log("You Hag XD");
+}
+sayHello("Yousef", 20, "male");
