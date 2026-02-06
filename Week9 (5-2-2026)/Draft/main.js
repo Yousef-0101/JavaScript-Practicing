04f4cfc7d2267a51fba118bf183bd4c889a7a6f3
Yousef <20912024100637@fci.zu.edu.eg>
2026-02-06 20:49:28 +0200

    Some notes on nested and arrow functions

--- Week9 (5-2-2026)/Draft/main.js
+++ Week9 (5-2-2026)/Draft/main.js
@@ -7,9 +7,59 @@ function sum(num1, num2) {
 console.log(sum(2, 9));
 
 // Anonymous Function can only be called after its declaration
+
 // console.log(subtract(2, 9)); error
 // Anonymous Function :
 let subtract = function (num1, num2) {
 	return num1 - num2;
 };
 console.log(subtract(2, 9));
+
+// Example 1
+function sayMessage(fName, lName) {
+	let message = "Hello";
+	// Nested Function
+	function concatMsg() {
+		message = `${message} ${fName} ${lName}`;
+	}
+	concatMsg();
+	return message;
+}
+console.log(sayMessage("Yousef", "Wahba"));
+
+// Example 2
+function sayMessage(fName, lName) {
+	let message = "Hello";
+	// Nested Function
+	function concatMsg() {
+		return `${message} ${fName} ${lName}`;
+	}
+	return concatMsg();
+}
+console.log(sayMessage("Yousef", "Wahba"));
+
+// Example 3
+function sayMessage(fName, lName) {
+	let message = "Hello";
+	// Nested Function
+	function concatMsg() {
+		// Nested Function
+		function getFullName() {
+			return `${fName} ${lName}`;
+		}
+		return `${message} ${getFullName()}`;
+	}
+	return concatMsg();
+}
+console.log(sayMessage("Yousef", "Wahba"));
+
+// Arrow Function
+let print1 = () => {
+	return 10;
+};
+console.log(print1());
+
+// If only one line you can write it like this:
+let print2 = () => 10;
+// let print2 = _ => 10; can also be done like this if there's no parameters
+console.log(print2());
