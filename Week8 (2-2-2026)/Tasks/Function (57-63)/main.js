de30cb41543c6b129bedb287a35f133374a03220
Yousef <20912024100637@fci.zu.edu.eg>
2026-02-06 16:50:50 +0200

    Moved function challenege to it's folder

--- Week8 (2-2-2026)/Tasks/Function (57-63)/main.js
+++ Week8 (2-2-2026)/Tasks/Function (57-63)/main.js
@@ -37,27 +37,6 @@ function ageInTime(theAge) {
 ageInTime(110); // Age Out Of Range
 ageInTime(38); // Months Example => 456 Months
 
-// T4
-function checkStatus(a, b, c) {
-	let job = [a, b, c];
-	let age;
-	let name;
-	let status;
-	for (let i = 0; i < job.length; i++) {
-		if (typeof job[i] === "boolean")
-			status = job[i]
-				? "You Are Available For Hire"
-				: "You Are Not Available For Hire";
-		else if (typeof job[i] === "number") age = job[i];
-		else if (typeof job[i] === "string") name = job[i];
-	}
-	console.log(`Hello ${name}, Your Age Is ${age}, ${status}`);
-}
-checkStatus("Osama", 38, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
-checkStatus(20, "Yousef", true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
-checkStatus(true, 25, "Ali"); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
-checkStatus(false, "Sameh", 44); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
-
 // T5
 function createSelectBox(startYear, endYear) {
 	document.write("<div>");
