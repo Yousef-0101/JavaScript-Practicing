fcf2514eecfaee98fa3fb5b8a077736018ec6091
Yousef <20912024100637@fci.zu.edu.eg>
2026-06-21 22:10:57 +0300

    Finished this week's tasks

--- Week17 (18-6-2026) Map And Set/Tasks/T7/main.js
+++ Week17 (18-6-2026) Map And Set/Tasks/T7/main.js
@@ -1,11 +1,11 @@
-let myInfo = {
-	username: "Osama",
-	role: "Admin",
-	country: "Egypt",
-};
+let numsOne = [1, 2, 3];
+let numsTwo = [4, 5, 6];
 
-let infoMap = new Map(Object.entries(myInfo));
+console.log([...numsOne, ...numsTwo]);
+console.log(numsOne.concat(numsTwo));
+console.log(new Array(...numsOne, ...numsTwo));
+console.log(Array.from(new Set([...numsOne, ...numsTwo])));
 
-console.log(infoMap);
-console.log(infoMap.size);
-console.log(infoMap.has("role"));
+let newArray = [...numsOne];
+newArray.push(...numsTwo);
+console.log(newArray);
